bd5da5beb7f45c252c6cbd2a867e5ad5c25e2b41
Saim Amir <saimxamir@gmail.com>
2023-03-23 21:12:21 -0400

    refactor setupInput() and play.js

---
+++ input.js
@@ -0,0 +1,23 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = () => {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+      process.stdout.write('.');
+    });
+  };
+
+  stdin.on("data",handleUserInput);
+
+  return stdin;
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,29 +1,6 @@
 const {connect} = require("./client.js"); //ES6 shorthand syntax.
-
+const setupInput = require("./input"); 
 console.log("Connection ...");
-
-// setup interface to handle user input from stdin
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = () => {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-      process.stdout.write('.');
-    });
-  };
-
-  stdin.on("data",handleUserInput);
-
-  return stdin;
-};
-
 
 connect();
 setupInput();
